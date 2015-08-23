cc1eb1dea37ac035951a063cd1d4065f997461b6
Dennis Lieu <dlieu@lieudennis.com>
2015-08-23 02:03:16 +0200

    Created a menu

---
+++ Assets/Scripts/HighscoreController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighscoreController : MonoBehaviour
+{
+    private static HighscoreController _instance;
+
+    #region Singleton
+    public static HighscoreController instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<HighscoreController>();
+
+                // Prevent Unity from destroying this object when loading another scene
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            // Make it a Singleton if this is the first instance
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            // In case there is another instance, destroy it
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    #endregion
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+#2015Shmup
+##Used Tutorials
+Various patterns are based on the tutorials that can be found here:
+[http://unitypatterns.com/](http://unitypatterns.com/ "Unity Patterns")
+
